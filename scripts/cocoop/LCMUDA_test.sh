3c19014feb7dc460f9552ecae9e21b9b0547e9ea
ptree789 <1597166181@qq.com>
2026-01-20 15:49:54 +0800

    Update LCMUDA_test.sh

--- scripts/cocoop/LCMUDA_test.sh
+++ scripts/cocoop/LCMUDA_test.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-cd /data1/ljs/CoOp
+cd /xxx/xxx/xxx
 
 # custom config
-DATA=/data1/ljs/CoOp/DATA
+DATA=/xxx/xxx/xxx/DATA
 TRAINER=LCMUDA
-# TRAINER=CoOp
 
 DATASET=officehome
 SEED=3
@@ -16,7 +15,7 @@ SHOTS=100
 
 
 #DIR=output/evaluation/${TRAINER}/${CFG}_${SHOTS}shots/${DATASET}/seed${SEED}
-DIR=output/evaluation/LCMUDA没跑完/${CFG}_${SHOTS}shots/${DATASET}/seed${SEED}
+DIR=output/evaluation/LCMUDA/${CFG}_${SHOTS}shots/${DATASET}/seed${SEED}
 if [ -d "$DIR" ]; then
     echo "Oops! The results exist at ${DIR} (so skip this job)"
 else
@@ -27,7 +26,7 @@ else
     --dataset-config-file configs/datasets/${DATASET}.yaml \
     --config-file configs/trainers/${TRAINER}/${CFG}.yaml \
     --output-dir ${DIR} \
-    --model-dir output/officehome/LCMUDA没跑完/${CFG}_${SHOTS}shots/seed${SEED} \
+    --model-dir output/officehome/LCMUDA/${CFG}_${SHOTS}shots/seed${SEED} \
     --source-domains art clipart product  \
     --target-domains real_world \
     --eval-only
